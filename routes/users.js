ec39c41e730e89b688c991b100e839e716f16b90
Allen Loshon <allenlosho913@gmail.com>
2021-04-24 17:51:03 -0700

    add route for users to be deleted

--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,23 @@ const User = require("../models/user");
 const express = require("express");
 const router = new express.Router();
 const { createToken } = require("../tokens");
+const { BadRequestError} = require("../expressError");
 const {ensureLoggedIn, ensureNotBlocked, ensureAdmin} = require("../auth");
 const userNewSchema = require("../schemas/userNew.json");
 
-/** GET [username]:  { username } => { user}
+const jsonschema = require("jsonschema");
+
+/** GET [username]:  { username } => { user }
  *
  * With the username in the parameter, get the user
  * from the database using that username.
  *
  * Returns { username, isAdmin }
  * 
- * Authorization required: logged in
+ * Authorization required: logged in, not blocked
  */
 
-router.get("/:username", ensureLoggedIn, async function (req, res, next) {
+router.get("/:username", ensureLoggedIn, ensureNotBlocked, async function (req, res, next) {
   try {
       const user = await User.get(req.params.username);
       return res.json({ user });
@@ -26,14 +29,14 @@ router.get("/:username", ensureLoggedIn, async function (req, res, next) {
     }
 });
 
-/** GET [username]/check:  { username } => { username }
+/** GET [username]/check:  { username } => { user }
  *
  * With the username in the parameter, get the user
  * from the database and return username.
  *
- * Returns { username }
+ * Returns { id, username }
  * 
- * Authorization required: logged in
+ * Authorization required: logged in, not blocked
  */
 
 router.get("/:username/check", ensureLoggedIn, ensureNotBlocked, async function (req, res, next) {
@@ -64,10 +67,26 @@ router.post("/", ensureAdmin, async function (req, res, next) {
       const errs = validator.errors.map(e => e.stack);
       throw new BadRequestError(errs);
     }
-
+    
     const user = await User.register(req.body);
     const token = createToken(user);
     return res.status(201).json({ user, token });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE / { username }  => { deleted: username }
+ *
+ * Deletes User
+ *
+ * Authorization required: admin or correct user
+ **/
+
+router.delete("/", ensureLoggedIn, async function (req, res, next) {
+  try {
+    await User.remove(res.locals.user.username);
+    return res.json({ deleted: res.locals.user.username });
   } catch (err) {
     return next(err);
   }
