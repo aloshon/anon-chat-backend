621fa129ca0611b6b74adb238f47a300151555fc
Allen Loshon <allenlosho913@gmail.com>
2021-05-23 12:22:25 -0700

    modify heroku worker

--- worker.js
+++ worker.js
@@ -3,22 +3,28 @@ const db = require("./db");
 let twoDaysAgoUTC = new Date(Date.now() - 2 * 24 * 60 * 60 * 1000);
 twoDaysAgoUTC.toUTCString();
 
-const deleteOldData = async () => {
+const deleteOldMessages = async () => {
     await db.query(
         `DELETE FROM chat_messages WHERE timestamp < $1`,[twoDaysAgoUTC]
     );
+}
 
+const deleteOldGuests = async () => {
     await db.query(
         `DELETE FROM guests as "g"
             LEFT JOIN group_chats AS "gc"
           ON gc.group_chat_id = g.group_chat_id
           WHERE gc.timestamp < $1`,[twoDaysAgoUTC]
     );
-
-    await db.query(
-        `DELETE FROM group_chats WHERE timestamp < $1`,[twoDaysAgoUTC]
-    )
 }
 
-deleteOldData();
+const deleteOldGroupChats = async () => {
+    await db.query(
+        `DELETE FROM group_chats WHERE timestamp < $1`,[twoDaysAgoUTC]
+    );
+}
+
+deleteOldMessages();
+deleteOldGuests();
+deleteOldGroupChats();
 process.exit();
