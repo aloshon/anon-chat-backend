b4b6aed8bdadae65883c33d43f3fa4318e60281b
Allen Loshon <allenlosho913@gmail.com>
2021-05-27 10:13:23 -0700

    remove console.logs

--- worker.js
+++ worker.js
@@ -1,31 +1,21 @@
 #!/usr/bin/env node
 const db = require("./db");
 const deleteOldData = async () => {
-    console.log("DID IT WORK?1")
     let twoDaysAgoUTC = new Date(Date.now() - 2 * 24 * 60 * 60 * 1000);
-    const res = await db.query(
-        `DELETE FROM chat_messages WHERE timestamp < $1
-        RETURNING user_id`,[twoDaysAgoUTC.toUTCString()]
+    await db.query(
+        `DELETE FROM chat_messages WHERE timestamp < $1`,[twoDaysAgoUTC.toUTCString()]
     );
-    console.log(twoDaysAgoUTC.toUTCString())
-    console.log(res.rows);
 
-    const gRes = await db.query(
+    await db.query(
         `DELETE FROM guests AS "g"
         USING group_chats AS "gc" WHERE
         g.group_chat_id = gc.id
-        AND gc.timestamp < $1
-        RETURNING g.user_id, gc.timestamp`,[twoDaysAgoUTC.toUTCString()]
+        AND gc.timestamp < $1`,[twoDaysAgoUTC.toUTCString()]
     );
-    console.log(gRes.rows);
 
-    const gcRes = await db.query(
-        `DELETE FROM group_chats WHERE timestamp < $1
-        RETURNING id`,[twoDaysAgoUTC.toUTCString()]
+    await db.query(
+        `DELETE FROM group_chats WHERE timestamp < $1`,[twoDaysAgoUTC.toUTCString()]
     );
-    console.log(gcRes.rows);
-
-    console.log("DID IT ALL WORK THO??")
 
     process.exit();
 }
