e69a023777c1805ad9897400b3e1a5cd650f2b62
Allen Loshon <allenlosho913@gmail.com>
2022-02-09 22:11:08 -0800

    add error handling

--- Database/anonChat.sql
+++ Database/anonChat.sql
@@ -1,17 +1,17 @@
 \echo 'Delete and recreate anon_chat db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE anon_chat;
+DROP DATABASE IF EXISTS anon_chat;
 CREATE DATABASE anon_chat;
 \connect anon_chat
 
 \i anonChat-schema.sql
-\i anonChat-seed.sql
+-- \i anonChat-seed.sql
 
 \echo 'Delete and recreate anon_chat_test db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE anon_chat_test;
+DROP DATABASE IF EXISTS anon_chat_test;
 CREATE DATABASE anon_chat_test;
 \connect anon_chat_test
 
