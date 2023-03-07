2430b1dd3a4aa7c6a36e80bd33561037873ceb77
Allen Loshon <allenlosho913@gmail.com>
2023-03-07 09:35:19 -0800

    fix flight error with cors hopefully part 9

--- app.js
+++ app.js
@@ -29,21 +29,7 @@ const app = express();
 //   origin: "*"
 // }));
 //Cors Configuration - Start
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
+app.use(cors());
 //Cors Configuration - End
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
